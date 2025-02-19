28712808766fe65a2e3e382350621ba481d31117
Nathan Tran <nathantran044@gmail.com>
2025-02-18 21:09:44 -0600

    started working on nav bar front end

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./NavBar.css";
 
 function Navbar() {
   const location = useLocation();
 
   if (location.pathname === "/game") return null; // Hide on /game
 
+  const isActive = (path) => location.pathname === path;
+
   return (
     <header className="App-header">
       <h1>Thirteen</h1>
       <nav>
-        {/* <Link to="/">Home</Link> | <Link to="/lobby">Lobby</Link> */}
+        <Link to="/lobby" className={isActive("/") ? "active" : ""}>
+          Lobby
+        </Link>
       </nav>
     </header>
   );
